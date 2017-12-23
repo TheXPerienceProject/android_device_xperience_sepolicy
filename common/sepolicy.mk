0606a1f650fab2e49138065b93195ed7e0d81e02
Demon000 <demonsingur@gmail.com>
2017-12-23 15:07:01 +0200

    sepolicy: common: add rules for lineage vibrator hal and some changes for xperience
    Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773
    Signed-off-by: klozz <klozz707@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from XPerience taken from Lineage
 #
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/xperience/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/xperience/sepolicy/common/private
+
 BOARD_SEPOLICY_DIRS += \
-    device/xperience/sepolicy/common
+   device/xperience/sepolicy/common/vendor
