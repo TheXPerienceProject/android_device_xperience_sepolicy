839e47a76c9dae84c91223e19716c20ce0a1627a
LuK1337 <priv.luk@gmail.com>
2018-02-09 15:43:11 +0100

    sepolicy: common: Lol three spaces
    Change-Id: Ia420ce23968cd1e245ca8272f5377725658f1f5c
    Signed-off-by: klozz <klozz707@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,4 +10,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/xperience/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-   device/xperience/sepolicy/common/vendor
+    device/xperience/sepolicy/common/vendor
