ceea5422f1b95629b7a8f1da8671950bb9b22f70
klozz <shaclone707@gmail.com>
2018-09-16 00:39:15 -0400

    sepolicy: fix our sepolicy inclusion
    Signed-off-by: klozz <shaclone707@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -7,5 +7,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/xperience/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/xperience/sepolicy/qcom \
+    device/xperience/sepolicy/qcom/common \
     device/xperience/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
