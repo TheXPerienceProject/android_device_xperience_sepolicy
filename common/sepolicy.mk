c41256b7e9e9792f374301fb0d84b2d7ff65c3b2
klozz <klozz@TheXPerienceProject.org>
2021-03-30 09:49:24 +0200

    sepol fixxes?
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,11 +1,17 @@
 #
 # This policy configuration will be used by all products that
-# inherit from XPerience taken from Lineage
+# inherit from xperience
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
 ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR),true)
+BOARD_SEPOLICY_M4DEFS += board_excludes_fuseblk_sepolicy=true
 endif
 endif
 
@@ -18,12 +24,11 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/xperience/sepolicy/common/dynamic \
-    device/xperience/sepolicy/common/system-only
+    device/xperience/sepolicy/common/system
 else
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xperience/sepolicy/common/dynamic \
     device/xperience/sepolicy/common/vendor
 endif
-
 # Selectively include legacy rules defined by the products
 -include device/xperience/sepolicy/legacy-common/sepolicy.mk
