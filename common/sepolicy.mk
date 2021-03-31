0e9de22a5a51bf7ed0a8c9c91afb9a2e54f58670
klozz <klozz@TheXPerienceProject.org>
2021-03-31 15:48:19 +0200

    sepolicy: remove updater data sepolicy idk why is not getting used and iour updater cant download files inside data/xperience_updates track this from device tree instead Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -30,5 +30,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xperience/sepolicy/common/dynamic \
     device/xperience/sepolicy/common/vendor
 endif
+
 # Selectively include legacy rules defined by the products
 -include device/xperience/sepolicy/legacy-common/sepolicy.mk
